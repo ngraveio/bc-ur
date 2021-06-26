1a1ff6d9a5479b2a7c6a480ab3c55c49c38336ce
Antonis Poulakis <antwnic4@gmail.com>
2021-06-26 22:55:31 +0300

    refactor: replace big-integer with js native BigInt

--- src/xoshiro.ts
+++ src/xoshiro.ts
@@ -1,62 +1,57 @@
 import { sha256Hash } from "./utils";
 import BigNumber from 'bignumber.js'
-import bigInt, { BigInteger } from 'big-integer'
 
 const MAX_UINT64 = 0xFFFFFFFFFFFFFFFF;
-const rotl = (x: BigInteger, k: number): BigInteger => asUintN(64, x.shiftLeft(k))
-  .or(
+const rotl = (x: bigint, k: number): bigint => asUintN(64, x << BigInt(k))
+   | BigInt(
     asUintN(
       64,
-      x.shiftRight((bigInt(64).minus(k)))
+      x >> (BigInt(64)- BigInt(k))
     )
   );
 
-const asUintN = (bits: number, bigint: BigInteger) => {
-  const p2bits = bigInt(1).shiftLeft(bits);
-  const mod = bigint.and(p2bits.subtract(1));
+const asUintN = (bits: number, bigint: bigint) => {
+  const p2bits = BigInt(1) << BigInt(bits);
+  const mod = bigint & BigInt(p2bits - BigInt(1));
   return mod
 };
 
 export default class Xoshiro {
-  private s: BigInteger[];
+  private s: bigint[];
 
   constructor(seed: Buffer) {
     const digest = sha256Hash(seed);
 
-    this.s = [bigInt(0), bigInt(0), bigInt(0), bigInt(0)];
+    this.s = [BigInt(0), BigInt(0), BigInt(0), BigInt(0)];
     this.setS(digest);
   }
 
   private setS(digest: Buffer) {
     for (let i = 0; i < 4; i++) {
       let o = i * 8;
-      let v = bigInt(0);
+      let v = BigInt(0);
       for (let n = 0; n < 8; n++) {
-        v = asUintN(64, v.shiftLeft(8));
-        v = asUintN(64, v.or(digest[o + n]));
+        v = asUintN(64, v << BigInt(8));
+        v = asUintN(64, v | BigInt(digest[o + n]));
       }
       this.s[i] = asUintN(64, v);
     }
   }
 
-  private roll(): BigInteger {
+  private roll(): bigint {
     const result = asUintN(
       64,
-      rotl(
-        asUintN(64, this.s[1].multiply(5)),
-        7
-      )
-        .multiply(9)
+      rotl(asUintN(64, this.s[1] * BigInt(5)), 7) * BigInt(9)
     );
 
-    const t = asUintN(64, this.s[1].shiftLeft(17));
+    const t = asUintN(64, this.s[1] << BigInt(17));
 
-    this.s[2] = asUintN(64, this.s[2].xor(this.s[0]));
-    this.s[3] = asUintN(64, this.s[3].xor(this.s[1]));
-    this.s[1] = asUintN(64, this.s[1].xor(this.s[2]));
-    this.s[0] = asUintN(64, this.s[0].xor(this.s[3]));
+    this.s[2] = asUintN(64, this.s[2] ^ BigInt(this.s[0]));
+    this.s[3] = asUintN(64, this.s[3] ^ BigInt(this.s[1]));
+    this.s[1] = asUintN(64, this.s[1] ^ BigInt(this.s[2]));
+    this.s[0] = asUintN(64, this.s[0] ^ BigInt(this.s[3]));
 
-    this.s[2] = asUintN(64, this.s[2].xor(t));
+    this.s[2] = asUintN(64, this.s[2] ^ BigInt(t));
 
     this.s[3] = asUintN(64, rotl(this.s[3], 45));
 
