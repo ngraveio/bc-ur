739ffe8b535d920e117c9c15375a71eeb10433bd
Pieter Uyttersprot <pieter.uyttersprot@ngrave.io>
2023-04-25 16:33:31 +0200

    removed transcoderFactory

--- tests/NgraveTranscoder.test.ts
+++ tests/NgraveTranscoder.test.ts
@@ -1,35 +1,30 @@
 import { NgraveTranscoder } from "../src/classes/Transcoder";
-import { TranscoderFactory } from "../src/classes/TranscoderFactory";
 import { Ur } from "../src/classes/Ur";
 
-describe("TranscoderFactory", () => {
-  test("encoderFactory creates default Transcoder", () => {
-    const transcoder = TranscoderFactory.create();
-    expect(transcoder).toBeInstanceOf(NgraveTranscoder);
-  });
+describe("NgraveTranscoder", () => {
   test("encoder encode/decode a primitive value", () => {
     const input = "test";
-    const { encoder, decoder } = TranscoderFactory.create();
+    const { encoder, decoder } = new NgraveTranscoder();
     const encodedPayload = encoder.encode(input);
     const decodedPayload = decoder.decode(encodedPayload);
     expect(decodedPayload).toEqual(input);
   });
   test("encoder encode/decode an object", () => {
     const input = { value: "this is a test value" };
-    const { encoder, decoder } = TranscoderFactory.create();
+    const { encoder, decoder } = new NgraveTranscoder();
     const encodedPayload = encoder.encode(input);
     const decodedPayload = decoder.decode(encodedPayload);
     expect(decodedPayload).toEqual(input);
   });
   test("encoder encode/decode an array", () => {
     const input = [1, 2, 3, 4, 5];
-    const { encoder, decoder } = TranscoderFactory.create();
+    const { encoder, decoder } = new NgraveTranscoder();
     const encodedPayload = encoder.encode(input);
     const decodedPayload = decoder.decode(encodedPayload);
     expect(decodedPayload).toEqual(input);
   });
-  test("TranscoderFactory creates Fountain encoder", () => {
-    const transcoder = TranscoderFactory.create();
+  test("Transcoder creates Fountain encoder", () => {
+    const transcoder = new NgraveTranscoder();
     const { fountainEncoderCreator } = transcoder;
     expect(fountainEncoderCreator).toBeInstanceOf(Function);
 
