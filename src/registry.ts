5c8d3d55f1c39dc275f590a7fd1c4e8d45488946
irfan798 <irfanbilaloglu@gmail.com>
2024-11-08 18:29:13 +0300

    chore: cleanup

--- src/registry.ts
+++ src/registry.ts
@@ -1,14 +1,13 @@
-import { CborTest, CoinInfo, myText, User, UserCollection } from "./classes/SomeItems.js";
 import { RegistryItemClass } from "./classes/RegistryItem.js";
 
 // TODO: singleton class?
 // Add items
 // Query items etc...
+// When adding items, check if the item is already in the registry
+// If it is show warning for overwriting
+// Add multiple items at once
+// Query items by tag, URType
+// Make sure CBOR2 tag registry is updated
+
 export type Registry = { [type: string]: RegistryItemClass } 
-export const registry: Registry = {
-  "user": User,
-  "user-collection": UserCollection,
-  "CborTest": CborTest,
-  "coininfo": CoinInfo,
-  "myText": myText,
-};
+export const registry: Registry = {};
