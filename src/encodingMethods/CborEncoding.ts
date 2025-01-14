0cb938462e7c318595636dda707e34b73417a2f2
irfan798 <irfanbilaloglu@gmail.com>
2025-01-14 21:22:38 +0300

    feat: Cbor enforce registry class

--- src/encodingMethods/CborEncoding.ts
+++ src/encodingMethods/CborEncoding.ts
@@ -1,4 +1,4 @@
-import { URRegistry, globalUrRegistry } from "../registry.js";
+import { URRegistryClass, UrRegistry } from "../registry.js";
 import { RegistryItem, RegistryItemClass, registryItemFactory } from "../classes/RegistryItem.js";
 import { EncodingMethodName } from "../enums/EncodingMethodName.js";
 import { IEncodingMethod } from "../interfaces/IEncodingMethod.js";
@@ -10,13 +10,13 @@ import {
 } from "../wrappers/cbor2.js";
 
 interface inputOptions {
-  registry?: URRegistry;
+  registry?: URRegistryClass;
   cborLibEncoderOptions?: EncodeOptions & {ignoreTopLevelTag?: boolean};
   cborLibDecoderOptions?: DecodeOptions;
 }
 
 interface decodeOptions {
-  enforceType?: RegistryItemClass;
+  enforceType?: RegistryItemClass | string;
   cborLibOptions?: DecodeOptions;
 }
 
@@ -33,7 +33,7 @@ export class CborEncoding<T extends RegistryItem>
   implements IEncodingMethod<T, Uint8Array>
 {
   private _name: EncodingMethodName = EncodingMethodName.cbor;
-  public registry: URRegistry = globalUrRegistry;
+  public registry: URRegistryClass = UrRegistry;
 
   /** Decoding options for CBOR2 library */
   cborLibEncoderOptions: EncodeOptions;
@@ -88,38 +88,57 @@ export class CborEncoding<T extends RegistryItem>
     const decoded = decode(payload, combinedOptions);
 
     // Check if enforce type is given, if so then give the value to the enforced type
-    // Unless it is already given type
-    if (enforceType && !(decoded instanceof enforceType)) {
-      // If we already have a tagged instance
-      // then we need to check if the tag matches the enforced type
-      if (decoded instanceof Tag) {
-        if (decoded.tag !== enforceType.tag) {
-          throw new Error(
-            `Enforced type does not match the tag of ${enforceType.URType}:${enforceType.tag} !== ${decoded.tag}`
-          );
-        }
-        // Try to create the instance of the enforced type from tag contents
-        return enforceType.fromCBORData(
-          decoded.contents,
-          enforceType.allowKeysNotInMap
-        ) as unknown as T;
+    if (enforceType) {
+      let foundType: RegistryItemClass;
+      // If enforceType is string then find the type from the registry
+      if (typeof enforceType === "string") {
+        foundType = this.registry.queryByURType(enforceType);
       }
-      // Try to create the instance of the enforced type from decoded data
-      return enforceType.fromCBORData(
-        decoded,
-        enforceType.allowKeysNotInMap
-      ) as unknown as T;
+      else {
+        foundType = enforceType;
+      }
+
+      // If we have a registry item then force the type
+      if (foundType) {
+        return forceType(decoded, foundType) as unknown as T;
+      }
     }
 
-    // If we still have a cbor tag but not resgistry item
+    // If we still have a cbor tag but not registry item then return try to convert it to registry item
     if (decoded instanceof Tag) {
       return tag2registryItem(decoded) as unknown as T;
     }
 
-
+    // Otherwise just return the decoded value
     // TODO: fix as unknown as T;
     return decoded as unknown as T;
   }
+}
+
+/**
+ * Force the data to be of the enforced type registry item
+ * @param data 
+ * @param enforceType 
+ * @returns 
+ */
+function forceType(data: any, enforceType: RegistryItemClass): RegistryItem {
+  // If its already the enforced type then return
+  if (data instanceof enforceType) {
+    return data;
+  }
+
+  // If data is CBOR tag item, try to convert it to enforced type
+  if (data instanceof Tag) {
+    if (data.tag !== enforceType.tag) {
+      throw new Error(
+        `Enforced type does not match the tag of ${enforceType.URType}:${enforceType.tag} !== ${data.tag}`
+      );
+    }
+    return enforceType.fromCBORData(data.contents, enforceType.allowKeysNotInMap);
+  }
+
+  // If data is raw then try to convert it to enforced type
+  return enforceType.fromCBORData(data, enforceType.allowKeysNotInMap);
 }
 
 /**
@@ -131,7 +150,7 @@ function tag2registryItem(unkownTag: Tag): RegistryItem {
   const tag = unkownTag.tag;
   const data = unkownTag.contents;
 
-  const registryItem = globalUrRegistry.queryByTag(tag);
+  const registryItem = UrRegistry.queryByTag(tag);
   if (registryItem) {
     return registryItem.fromCBORData(
       data,
